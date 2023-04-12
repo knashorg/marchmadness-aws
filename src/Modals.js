3082e14773a4c9bb0cc188cd2ded9e90e0d925af
nicmoffett <nicholas.moffett@student.lovett.org>
2023-04-12 10:17:14 -0400

    Update Modals.js
    finished putting in team info

--- src/Modals.js
+++ src/Modals.js
@@ -15,8 +15,8 @@ export function VerticallyCenteredModal1(props) {
       </Modal.Header>
       <Modal.Body>
         <h4>27 - 10 - 0</h4>
-        <p> 38 tournament appearances
-            Best Finish - Champions (5x)
+        <p> 38 tournament appearances /
+            Best Finish - Champions (5x) /
             Head Coach - Jay Pandolfo
 </p>
       </Modal.Body>
@@ -40,8 +40,8 @@ export function VerticallyCenteredModal2(props) {
       </Modal.Header>
       <Modal.Body>
         <h4>20 - 18 - 3</h4>
-        <p> 2 tournament appearances
-            Best Finish - DNP
+        <p> 2 tournament appearances /
+            Best Finish - DNP /
             Head Coach - Trevor Large
 </p>
       </Modal.Body>
@@ -65,8 +65,8 @@ export function VerticallyCenteredModal3(props) {
       </Modal.Header>
       <Modal.Body>
         <h4>19 - 15 - 5</h4>
-        <p> 6 tournament appearances
-            Best Finish - Runner-up
+        <p> 6 tournament appearances /
+            Best Finish - Runner-up /
             Head Coach - Don Vaughn
 </p>
       </Modal.Body>
@@ -90,8 +90,8 @@ export function VerticallyCenteredModal4(props) {
       </Modal.Header>
       <Modal.Body>
         <h4>20 - 10 - 2</h4>
-        <p> 23 tournament appearances
-            Best Finish - Champions (2x)
+        <p> 23 tournament appearances /
+            Best Finish - Champions (2x) /
             Head Coach - Mike Schafer
 </p>
       </Modal.Body>
@@ -115,8 +115,8 @@ export function VerticallyCenteredModal5(props) {
       </Modal.Header>
       <Modal.Body>
         <h4>30 - 9 - 0</h4>
-        <p> 32 tournament appearances
-            Best Finish - Champions (9x)
+        <p> 32 tournament appearances /
+            Best Finish - Champions (9x) /
             Head Coach - David Carle
 </p>
       </Modal.Body>
@@ -136,11 +136,14 @@ export function VerticallyCenteredModal6(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 6</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Harvard</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>24 - 7 - 2</h4>
+        <p> 27 tournament appearances /
+            Best Finish - Champions /
+            Head Coach - Ted Donato
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -158,11 +161,14 @@ export function VerticallyCenteredModal7(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 7</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Merrimack</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>23 - 13 - 1</h4>
+        <p> A3 tournament appearances /
+            Best Finish - DNP /
+            Head Coach - Scott Borek
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -180,11 +186,14 @@ export function VerticallyCenteredModal8(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 8</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Michigan</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>24 - 11 - 3</h4>
+        <p> 40 tournament appearances /
+            Best Finish - Champions (9x) /
+            Head Coach - Brandon Naurato (interim)
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -202,11 +211,14 @@ export function VerticallyCenteredModal9(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 9</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Michigan Tech</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>24 - 10 - 4</h4>
+        <p> 15 tournament appearances /
+            Best Finish - Champions (3x) /
+            Head Coach - Joe Shawhan
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -224,11 +236,14 @@ export function VerticallyCenteredModal10(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 10</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Minnesota</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>26 - 9 - 1</h4>
+        <p> 40 tournament appearances /
+            Best Finish - Champions (5x) /
+            Head Coach - Bob Motzko
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -246,11 +261,14 @@ export function VerticallyCenteredModal11(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 11</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Minnesota State</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>25 - 12 - 1</h4>
+        <p> 9 tournament appearances /
+            Best Finish - Runner-up /
+            Head Coach - Mike Hastings
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -268,11 +286,14 @@ export function VerticallyCenteredModal12(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 12</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Ohio State</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>20 - 15 - 3</h4>
+        <p> 10 tournament appearances /
+            Best Finish - Frozen Four (2x) /
+            Head Coach - Steve Rohlik
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -290,11 +311,14 @@ export function VerticallyCenteredModal13(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 13</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Penn State</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>21 - 15 -1</h4>
+        <p> 3 tournament appearances /
+            Best Finish - DNP /
+            Head Coach - Guy Gadowski
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -312,11 +336,14 @@ export function VerticallyCenteredModal14(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 14</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Quinnipiac</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>30 - 4 - 3</h4>
+        <p> 9 tournament appearances /
+            Best Finish - Runner-up (2x) /
+            Head Coach - Rand Pecknold
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -334,11 +361,14 @@ export function VerticallyCenteredModal15(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 15</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">St. Cloud State</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>24 - 12 - 3</h4>
+        <p> 17 tournament appearances /
+            Best Finish - Runner-up /
+            Head Coach - Brett Larson
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -356,11 +386,14 @@ export function VerticallyCenteredModal16(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Team 16</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Western Michigan</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Record</h4>
-        <p>About this team.</p>
+        <h4>23 - 14 - 1</h4>
+        <p> 8 tournament appearances /
+            Best Finish - DNP /
+            Head Coach - Pat Ferschweiler
+</p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
